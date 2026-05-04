0fe589bc9c97a6be526ade114aaf2cc6f4b34c37
Adrián Gil Reyes <gilreeyes.5@gmail.com>
2026-05-04 18:00:57 +0100

    añadimos estructura

--- tasks.py
+++ tasks.py
@@ -9,21 +9,22 @@ id_task = 0
 option = 0
 
 while True:
-
+    print('---------------')
     print('1. Añadir tarea')
     print('2. Eliminar tarea')
     print('3. Listar tareas')
     print('4. Salir')
-
+    print('----------------') 
     option = int(input('Introduce una opción:'))
 
     if option == 1:
+        user_name = input('Introduce un nombre:')
         id_task = id_task + 1
         name_task = input('Introduce el nombre de la tarea:')
         category_task = input('Introduce la categoría de la tarea:')
         priority_task = input('Introduce la prioridad de la tarea:')
         hour = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-        tasks[id_task] = {"id":id_task, "name_task":name_task,"category_task":category_task,"priority_task":priority_task,
+        tasks[id_task] = {"user_name":user_name,"id":id_task, "name_task":name_task,"category_task":category_task,"priority_task":priority_task,
         "datetime:":hour}
     
     elif option == 2:
@@ -35,5 +36,5 @@ while True:
             print(task)
     
     elif option == 4:
-        print('Saliendo....')
+        print('Saliendo....',user_name) 
         exit()
